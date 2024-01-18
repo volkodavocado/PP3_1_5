0eee463e182e2bfab5466915884ffe6f7938d797
volkodavocado <olga.braulova@gmail.com>
2024-01-19 01:13:08 +0300

    fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,8 +2,8 @@ insert into roles (name)
 values
 ('ROLE_USER'), ('ROLE_ADMIN');
 
-insert into users (password, username)
+insert into users (password, email)
 values
-('$2a$12$JSYjkbR.SUVQpn/Vp7Y3bOD2.IsS17QvRqCtjK9OATUo/rVdFX/Ym', 'user');
+('$2a$12$JSYjkbR.SUVQpn/Vp7Y3bOD2.IsS17QvRqCtjK9OATUo/rVdFX/Ym', 'user@mail.ru');
 
 insert into users_roles (user_id, role_id) values (1, 2);
